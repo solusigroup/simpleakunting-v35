4e2c8d3ad5060f21a136b42228a10084d8fdf0c0
solusigroup <kurniawan@petalmail.com>
2026-01-01 12:53:25 +0700

    Fix database password (restored missing # character)

--- app/config.php
+++ app/config.php
@@ -11,5 +11,5 @@ define('APPROOT', dirname(dirname(__FILE__)));
 // Konfigurasi Database (pastikan ini sesuai dengan database di hosting Anda)
 define('DB_HOST', 'localhost');
 define('DB_USER', 'simpleak_user_simple');
-define('DB_PASS', '5@8@12Yaa');
+define('DB_PASS', '#5@8@12Yaa');
 define('DB_NAME', 'simpleak_db_akunting');
